a851473630507f0ea087477824812da6d277a53b
uimarshall <uimarshall@gmail.com>
2023-06-28 13:56:01 +0100

    Add Navbar and the Feed component.

--- app/page.jsx
+++ app/page.jsx
@@ -4,6 +4,8 @@
  * Whenever you're utilizing state (When managing state in the app) in your app or other client side management solutions,, for instance you want to use React useState or any other hook, you need to use CSR or declare the component as a Client Component - by 'use client'; at the top of the file or component because the state of the application always changes or updated in the browser. State management in React is primarily a client side thing. The component state is managed and updated within the browser.
  */
 
+import Feed from '@components/Feed';
+
 export default function Home() {
   return (
     <section className="w-full flex-col flex-center">
@@ -15,6 +17,7 @@ export default function Home() {
         FAQprompters is an AI prompting tool for modern world to discover, and
         share FAQ with Prompts
       </p>
+      <Feed />
     </section>
   );
 }
